8aaef59e1ab172314519409dc3fe5077a340c462
OssianVRmz <vladiss1234rmz@hotmail.com>
2025-01-27 22:34:15 -0600

    Print from 1 to 10 addittion.
    Now the code print from the number 1 through 10 (integers). There is also a printed line that serves as a reminder that the code is inefficient.

--- prueba1.py
+++ prueba1.py
@@ -1,1 +1,13 @@
 print ("Hello World")
+
+print ("Imprimire de una manera ineficiente los numeros del 1 al 10")
+print ("1")
+print ("2")
+print ("3")
+print ("4")
+print ("5")
+print ("6")
+print ("7")
+print ("8")
+print ("9")
+print ("10")
